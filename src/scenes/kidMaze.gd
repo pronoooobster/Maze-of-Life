1ea31fae493728b8551dbd05d27d94a449c45eae
Andrii Demchenko <demch.andrew@gmail.com>
2024-03-23 16:31:05 +0100

    Added scene switch

---
+++ src/scenes/kidMaze.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _on_endgame_collider_body_entered(body):
+	# chceck if the player has entered
+	if body.is_in_group("Player"):
+		call_deferred("switch_to_new_scene")
+
+# Function to switch to a new scene
+func switch_to_new_scene():
+	# Change the scene
+	get_tree().change_scene_to_file("res://scenes/teenMaze.tscn")
