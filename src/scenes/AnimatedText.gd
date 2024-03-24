f23a9777a50518066be1066bd8e4709d4f5a2ae1
Andrii Demchenko <demch.andrew@gmail.com>
2024-03-24 15:00:32 +0100

    stashing

--- src/scenes/AnimatedText.gd
+++ src/scenes/AnimatedText.gd
@@ -5,14 +5,9 @@ func _process(delta):
 	pass
 
 func _ready():
-	#$Label.percentage_visible = 0
-	$Area2D.body_entered.connect(_play_animation)  
-	#$Area2D.connect('body_entered', self, '_play_animation')
-
-
-func _play_animation(body):
-	print('show text')
-	#$Area2D.body_entered.Disconnect(_play_animation)  
-	#$Area2D.disconnect('body_entered', self, '_play_animation')
 	$AnimationPlayer.play('new_animation')
-	
+
+
+func _on_animation_player_animation_finished(anim_name):
+	#load the kid scene
+	get_tree().change_scene_to_file("res://scenes/kidMaze.tscn")
