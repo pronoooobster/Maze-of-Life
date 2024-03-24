477b222d76dd3296c72fd33ae017c16973e15bcb
Andrii Demchenko <demch.andrew@gmail.com>
2024-03-24 16:18:59 +0100

    added death after adult

--- src/scenes/adultMaze.gd
+++ src/scenes/adultMaze.gd
@@ -9,3 +9,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_finish_body_entered(body):
+		# chceck if the player has entered
+	if body.is_in_group("Player"):
+		call_deferred("switch_to_new_scene")
+
+# Function to switch to a new scene
+func switch_to_new_scene():
+	# Change the scene
+	get_tree().change_scene_to_file("res://scenes/deathsc.tscn")
